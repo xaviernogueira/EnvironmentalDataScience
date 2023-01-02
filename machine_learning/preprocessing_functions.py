10c553f7dee0c2fb4b28fb984f8f3b6792664171
Xavier Nogueira <58796351+xaviernogueira@users.noreply.github.com>
2023-01-01 23:13:25 -0500

    Updating preprocessing functions to use pathlib instead of os, and have better typehints

--- machine_learning/preprocessing_functions.py
+++ machine_learning/preprocessing_functions.py
@@ -1,16 +1,20 @@
-import os
 import gc
+from pathlib import Path
 import numpy as np
 import pandas as pd
 import dask.dataframe as dd
 from typing import Union
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.impute import KNNImputer
-from typing import Dict
+from typing import Dict, List, Optional
 
 
-def knn_impute(df: pd.DataFrame, n_neighbors: int = 5, nodata_values: Union[float, int] = np.nan,
-               normalizer=MinMaxScaler()) -> pd.DataFrame:
+def knn_impute(
+    df: pd.DataFrame,
+    n_neighbors: int = 5,
+    nodata_values: Union[float, int] = np.nan,
+    normalizer=MinMaxScaler(),
+) -> pd.DataFrame:
     """
     Fill in missing values in a dataframe using K-Nearest Neighbors (KNN) imputation.
     Note: Using KNN imputation to fill in missing training set values can increase ML model performance.
@@ -29,26 +33,39 @@ def knn_impute(df: pd.DataFrame, n_neighbors: int = 5, nodata_values: Union[floa
         print(f'Error: Exception arose - {e}')
 
 
-def _id_csvs(data_dir: str) -> dict:
+def _id_csvs(data_dir: Union[Path, str]) -> dict:
     """
     Takes a dir path containing .csv tables, and returns a dict w/ the .csv names as keys, and paths as values.
     :param data_dir: (str path) a valid directory path containing >=1 .csv files.
     :return: dict of the form {'big_data': 'C:\\path_to_data\\big_data.csv', ...}.
     """
+
+    # handle path input
+    if isinstance(data_dir, str):
+        data_dir = Path(data_dir)
+    elif not isinstance(data_dir, Path):
+        raise TypeError(
+            'param:data_dir must be a pathlib.Path object or a string path!')
+    if not data_dir.is_dir():
+        raise ValueError(
+            f'param:data_dir = {data_dir} is not a directory or does not exist! ')
+
     csv_dict = {}
 
     # iterate over data_dir looking for .csv files
-    for file in os.listdir(data_dir):
-        if file[-4:] == '.csv':
-            # add to csv_dict
-            csv_dict[file.split('.', 2)[0]] = f'{data_dir}\\{file}'
+    for file in data_dir.iterdir():
+        if file.suffix == '.csv':
+            csv_dict[file.name.replace('.csv', '')] = file
 
     # print csv_dict items, and return the dictionary
     print(f'csv_dict initiated {csv_dict.keys()}')
     return csv_dict
 
 
-def _import_csv(csv_dict: dict, index_col: str = None) -> Dict[str: dd.DataFrame]:
+def _import_csv(
+    csv_dict: Dict[str, Path],
+    index_col: Optional[str] = None,
+) -> Dict[str, dd.DataFrame]:
     """
     Uses dask to partition into multiple dask.dataframes with max # of rows = chunk_size
     :param csv_dict: the output of id_csv
@@ -64,7 +81,9 @@ def _import_csv(csv_dict: dict, index_col: str = None) -> Dict[str: dd.DataFrame
 
         # if an index is provided, warn the user about the performance penalty
         if index_col is not None:
-            print(f'WARNING: Setting index to {index_col} will drastically hurt performance!')
+            print(
+                f'WARNING: Setting index to {index_col} will drastically hurt performance!'
+            )
             df = df.set_index(index_col)
 
         # update the dictionary and collect trash
@@ -74,9 +93,11 @@ def _import_csv(csv_dict: dict, index_col: str = None) -> Dict[str: dd.DataFrame
     return csv_dict
 
 
-def _object_to_cat(df,
-                   ignore_cols: list = None,
-                   override_cols: list = None) -> dd.DataFrame:
+def _object_to_cat(
+    df: dd.DataFrame,
+    ignore_cols: Optional[List[str]] = None,
+    override_cols: Optional[List[str]] = None,
+) -> dd.DataFrame:
     """
     Converts all object columns to category type in dask.dataframe, unless specified in ignore_cols
     :param df: (dask.DataFrame) as dask dataframe.
@@ -101,17 +122,19 @@ def _object_to_cat(df,
     return df
 
 
-def csvs_to_parquets(data_dir: str,
-                     ignore_cols: list = None,
-                     to_datetime_cols: dict = None,
-                     out_path: str = None) -> str:
+def csvs_to_parquets(
+    data_dir: Union[str, Path],
+    ignore_cols: Optional[List[str]] = None,
+    to_datetime_cols: Optional[Dict[str, str]] = None,
+    out_dir: Union[str, Path] = None,
+) -> str:
     """
     Converts very large CSV files into partitioned .parquet files in parallel using dask.
     :param data_dir: (str path) a valid directory path containing >=1 .csv files.
     :param ignore_cols: (list of str column headers, optional) dtype=object columns to NOT convert to category.
     :param to_datetime_cols: (list of str column headers, optional) columns to convert to datetime.
-    :param out_path: (str path, optional) a path to save the output partitioned .parquet files.
-    :return: (str path) out_path where the parquet files are stored.
+    :param out_dir: (str path, optional) a path to save the output partitioned .parquet files.
+    :return: (str path) out_dir where the parquet files are stored.
     """
     # get all csvs from param:data_dir a dictionary
     csv_dict = _id_csvs(data_dir)
@@ -119,7 +142,8 @@ def csvs_to_parquets(data_dir: str,
     # update csv_dict to store dask dataframes for each data type
     _import_csv(csv_dict=csv_dict)
     _ = gc.collect()
-    print(f'csv_dict stores dask.dataframes with the following keys: {csv_dict.keys()}')
+    print(
+        f'csv_dict stores dask.dataframes with the following keys: {csv_dict.keys()}')
 
     # convert all other object columns to category, and any columns to datetime using a format string
     for name, df in csv_dict.items():
@@ -132,21 +156,33 @@ def csvs_to_parquets(data_dir: str,
         csv_dict[name] = _object_to_cat(df, ignore_cols=ignore_cols)
     _ = gc.collect()
 
-    # define the output path automatically is param:out_path is not valid/provided
-    if not out_path or not os.path.exists(out_path):
-        out_path = os.path.join(data_dir, 'output_parquets')
+    # define the output path automatically is param:out_dir is not valid/provided
+    if out_dir is not None:
+        if isinstance(out_dir, str):
+            out_dir = Path(out_dir)
+        elif not isinstance(out_dir, Path):
+            raise TypeError(
+                'param:out_dir must be a pathlib.Path object or a string!'
+            )
+
+    if not out_dir or not out_dir.exists():
+        out_dir = Path(data_dir / 'output_parquets')
 
     for title, df in csv_dict.items():
-        out_path = out_path + f'\\{title}_parquets'
+        out_parq = out_dir / f'{title}.parquet'
 
         # convert the dask dataframe to parquet
-        dd.to_parquet(df, out_path, engine='pyarrow', compression='gzip')
-        print(f'{title} saved @ {out_path}')
+        dd.to_parquet(
+            df,
+            out_parq,
+            engine='pyarrow',
+            compression='gzip',
+        )
+        print(f'{title}.parquet saved @ {out_dir}')
 
         # collect garbage and delete the previous dask dataframe
         _ = gc.collect()
         del df
 
-    print(f'Done! all .parquet files saved @ {out_path}')
-    return out_path
-
+    print(f'Done! all .parquet files saved @ {out_dir}')
+    return out_dir
